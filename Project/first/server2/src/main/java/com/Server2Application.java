2cf4de5bd2b16613a6db8925eee486952cf94e25
fly <onemedusa@163.com>
2018-11-13 23:52:04 +0800

    fly: 增加了Feign基于ribbon实现负载均衡的演示项目

--- Project/first/server2/src/main/java/com/Server2Application.java
+++ Project/first/server2/src/main/java/com/Server2Application.java
@@ -2,13 +2,11 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class ServerApplication {
+public class Server2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(Server2Application.class, args);
     }
 }
