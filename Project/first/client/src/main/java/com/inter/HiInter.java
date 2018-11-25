2eb40dbab4a4d527b0f20a28be4050f06e1c21e7
fly <onemedusa@163.com>
2018-11-25 22:11:15 +0800

    fly: 增加了Feign的Hystrix断路器的支持

--- Project/first/client/src/main/java/com/inter/HiInter.java
+++ Project/first/client/src/main/java/com/inter/HiInter.java
@@ -1,13 +1,22 @@
 package com.inter;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "SERVICE-SERVER")
+@FeignClient(value = "SERVICE-SERVER",fallback = HiHy.class)
 public interface HiInter {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     String sayHello();
 
+}
+
+@Component
+class HiHy implements HiInter{
+    @Override
+    public String sayHello() {
+        return "Sorry,Service is error!";
+    }
 }
