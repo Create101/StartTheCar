2a11ca42650343c436222ea3c925361c65da99f1
fly <onemedusa@163.com>
2018-12-27 20:30:29 +0800

    fly: 添加了断路器的聚合服务

--- Project/first/config/src/main/java/com/ConfigApplication.java
+++ Project/first/config/src/main/java/com/ConfigApplication.java
@@ -4,14 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
+@RestController
 public class ConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+	@GetMapping(value = "/config")
+	public String sayZuul(){
+		return "这是Config!";
 	}
 
 }
--- Project/first/turbine/src/main/java/com/TurbineApplication.java
+++ Project/first/turbine/src/main/java/com/TurbineApplication.java
@@ -4,33 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-@EnableFeignClients
 @RestController
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableCircuitBreaker
-public class ClientApplication {
+@EnableTurbine
+public class TurbineApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
-    }
-
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineApplication.class, args);
+	}
 
 }
+
